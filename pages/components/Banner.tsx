1c7f9da4fa9dd54897bcfb30d57801375176b092
Adespscientist <79201846+Adespscientist@users.noreply.github.com>
2022-11-19 22:35:51 +0100

    Update Banner.tsx

--- pages/components/Banner.tsx
+++ pages/components/Banner.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image"
 import { useEffect, useState } from "react";
 import { baseUrl } from "../../constants/movie";
-import { Movie } from "../typings"
-import {FaPlay} from 'react-icons/fa'
+import { Movie } from "../typings";
+import {FaPlay} from 'react-icons/fa';
 import { InformationCircleIcon } from "@heroicons/react/solid";
 interface Props{
   netflixOriginals: Movie[];
